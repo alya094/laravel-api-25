c1b813a2247f9a263bca2aaf4f8dbf9152c7e970
alya094 <alyaef094@gmail.com>
2025-10-31 14:03:18 +0700

    okee

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -3,23 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+                // Urutan penting: Authors dulu, baru Books (karena foreign key)
+                $this->call([
+                    AuthorSeeder::class,
+                    BookSeeder::class,
         ]);
     }
-}
+} 
