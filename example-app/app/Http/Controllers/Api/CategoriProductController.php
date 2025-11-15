537c4317885752648664e223ac90aa0fe7db203e
Faisaleka21 <162295533+Faisaleka21@users.noreply.github.com>
2025-11-15 07:31:14 +0700

    Refactor CategoriProduct model and migration; update API routes to include CategoriProduct resource

---
+++ example-app/app/Http/Controllers/Api/CategoriProductController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\CategoriProduct;
+use Illuminate\Http\Request;
+
+
+class CategoriProductController extends Controller
+{
+    //
+    public function index(){
+        $category_product=CategoriProduct::all();
+        return response()->json($category_product);
+    }
+
+    public function store(Request $request){
+        $validateData=$request->validate([
+            'name'=>'required|max:255',
+            'description'=>'required|string',
+        ]);
+
+        $category_product=CategoriProduct::create($validateData);
+        return response()->json([
+            'status'=>'Succes',
+            'data'=>$category_product,
+            'message'=>'Berhasil disimpan',
+        ],201);
+    }
+
+    public function show($id){
+        $category=CategoriProduct::find($id);
+        if(!$category){
+            return response()->json([
+                'message'=>'Tidak ada data',
+                'data'=>null
+            ],401);
+        }
+    }
+
+    public function update(Request $request,$id){
+        $category=CategoriProduct::find($id);
+        $validate=$request->validate([
+            'name'=>'required|max:255',
+            'description'=>'required|string'
+        ]);
+        $category->update([
+            'name'=>$validate['name'],
+            'description'=>$validate['description'],
+        ]);
+        return response()->json([
+            'status'=>'succes',
+            'data'=>$category,
+            'message'=>'Berhasil diupdate',
+        ]);
+    }
+
+    public function destroy($id){
+        $category=CategoriProduct::find($id);
+        $category->delete();
+        return response([
+            'status'=>'Berhasil',
+            'data'=>$category,
+            'message'=>'Data berhasil dihapus!!!',
+        ],201);
+
+    }
+
+}
--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\Api\CategoriProductController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::resource('/categori-product',CategoriProductController::class);
+
+    Route::resource('/products', ProductController::class); 
+    Route::get('/products', [ProductController::class, 'index']);
+
+    Route::resource('vendors', VendorController::class);
+    Route::post('/products-store', [ProductController::class, 'store']);
+
+    Route::get('/books', [BookController::class, 'index']);
+    Route::get('/books/{id}', [BookController::class, 'show']);
+    
+    Route::get('/halo', function () {
+        return 'Halo, Laravel!';
+        });
+});
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
