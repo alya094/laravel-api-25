c88beea26eea21e1c4244daf7469bd002d17f58c
Faisaleka21 <162295533+Faisaleka21@users.noreply.github.com>
2025-11-21 10:44:12 +0700

    okkk

--- example-app/app/Http/Controllers/Api/CategoriProductController.php
+++ example-app/app/Http/Controllers/Api/CategoriProductController.php
@@ -35,7 +35,8 @@ class CategoriProductController extends Controller
             return response()->json([
                 'message'=>'Tidak ada data',
                 'data'=>null
-            ],401);
+            ],401); 
+            // okee
         }
     }
 
